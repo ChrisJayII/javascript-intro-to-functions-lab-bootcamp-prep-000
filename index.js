d4282ce27178b5af700ba7b8493e853e2464d391
Christpher <chrisoutrules@yahoo.com>
2019-01-04 02:35:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ function whisper(string) {
 function logshout(string){
   console.log(string.toUpperCase())
 }
+
 function logwhisper(string){
   console.log(string.toLowerCase())
 }
@@ -18,6 +19,6 @@ function sayHiToGrandma(string){
   return "I can\'t hear you!"}
   if (string === string.toUpperCase()){
   return "YES INDEED!"}
-  if (string === 'I love you Grandma.'){
+  if (string === 'I love you, Grandma.'){
   return "I love you, too."}
 }
